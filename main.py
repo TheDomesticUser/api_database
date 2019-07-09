ee5857484f1988a49115acc80cfcbc354f9bb95b
TheDomesticUser <thedomesticuser@gmail.com>
2019-07-08 23:39:28 -0400

    Update main.py
    Added code that calculates what values are in a JSON dictionary, and creates the set with them. 2019-07-08

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from data_transfer import DataTransfer
 # This function converts all string types (str), null values (NoneType), and dictionary types (dict) to VARCHAR 
 # with a length depending on the variable type
 def convertVarsToVarchar(string):
-    string = re.sub(r'(?:\bstr\b|\bNoneType\b)', 'VARCHAR(150)', string) # The VARCHAR length can be any value
-    string = re.sub(r'\bdict\b', 'VARCHAR(2000)', string) # The VARCHAR length can be any value
+    string = re.sub(r'(?:\bstr\b|\bNoneType\b)', 'VARCHAR(300)', string) # The VARCHAR length can be any value
+    string = re.sub(r'\bdict\b', 'VARCHAR(1000)', string) # The VARCHAR length can be any value
 
     return string
 
@@ -52,61 +52,63 @@ url_contents = requests.get(url).json()
 
 ###################################### CREATE THE TABLE #######################################################
 
-values = ''
-keySet = set()
+dictContents = ''
+itemDict = {}
 
-# Iterate through the dictionaries inside the list, and appending their keys to the key list set
+# Iterate through each dictionary inside the url contents, updating the items to the item dict, assuring all the keys are unique.
+# Change the value according to its type
 for dictionary in url_contents:
-    for key in dictionary:
-        keySet.add(key)
-print(keySet)
-# # Iterate through the key sets, appending the key name and type of the key to the value string
+    for key, value in dictionary.items():
+        itemDict.update({ key: type(value).__name__ })
 
+# Iterate through the item dict, appending the key and values to the dictContents string
+for key, value in itemDict.items():
+    dictContents += f'{key} {value},\n'
 
-# # Remove the comma and newline at the end of values
-# values = values[:-2]
+# Remove the comma and newline at the end of values
+dictContents = dictContents[:-2]
 
-# # Convert all str's and dict's to VARCHAR(1000) in values
-# values = convertVarsToVarchar(values)
+# Convert all str's and dict's to VARCHAR(1000) in values
+dictContents = convertVarsToVarchar(dictContents)
 
-# # Create the SQL command for creating the table
-# createTableCommand = f'''
-# CREATE TABLE {databaseName}.{tableName}
-# (
-# {values}
-# )
-# '''
+# Create the SQL command for creating the table
+createTableCommand = f'''
+CREATE TABLE {databaseName}.{tableName}
+(
+{dictContents}
+)
+'''
 
-# # Start the connection with MYSQL
-# dataTransfer = DataTransfer('localhost', 3306, 'root', 'incorrect')
+# Start the connection with MYSQL
+dataTransfer = DataTransfer('localhost', 3306, 'root', 'incorrect')
 
-# # Create the table
-# dataTransfer.insertData(createTableCommand)
+# Create the table
+dataTransfer.insertData(createTableCommand)
 
-# ############################## INSERT THE JSON DATA INTO THE TABLE ########################################
+############################## INSERT THE JSON DATA INTO THE TABLE ########################################
 
-# keys = ''
+# Iterate through the url_contents, getting their values and adding them in the MYSQL database
+for data in url_contents:
+    keys = ''
 
-# # Iterate through the url contents with index 0, getting the keys and separating them with commas
-# for key in propertyList:
-#     keys += f'{key}, '
+    # Iterate through the url contents with index 0, getting the keys and separating them with commas
+    for key in data:
+        keys += f'{key}, '
 
-# # Remove the extra comma and whitespace at the end of keys
-# keys = keys[:-2]
+    # Remove the extra comma and whitespace at the end of keys
+    keys = keys[:-2]
 
-# # Iterate through the url_contents, getting their values and adding them in the MYSQL database
-# for data in url_contents:
-#     dictValue = ''
-#     for value in data.values():
-#         dictValue += f'{insertQuotations(objectSeparator(value))}, '
-#     # Remove the comma and extra whitespace at the end of dictValue
-#     dictValue = dictValue[:-2]
+    dictValue = ''
+    for value in data.values():
+        dictValue += f'{insertQuotations(objectSeparator(value))}, '
+    # Remove the comma and extra whitespace at the end of dictValue
+    dictValue = dictValue[:-2]
     
-#     # Prepare the SQL statement
-#     insertDataStatement = f'''
-#     INSERT INTO {databaseName}.{tableName}({keys})
-#     VALUES ({dictValue})
-#     '''
+    # Prepare the SQL statement
+    insertDataStatement = f'''
+    INSERT INTO {databaseName}.{tableName}({keys})
+    VALUES ({dictValue})
+    '''
 
-#     # Execute the statement
-#     dataTransfer.insertData(insertDataStatement)
+    # Execute the statement
+    dataTransfer.insertData(insertDataStatement)
